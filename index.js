4e16d6e3395c78784ab948e4533f836342fb0315
Raymond Martinez <rcmartinez1854@gmail.com>
2019-08-01 18:07:56 -0700

    added scroll function

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-// function fadeInHeroHeading() {
-//     (function () {
-// 		$('.introduction h1').fadeIn(500, function () {
-// 			$('.introduction h2').delay(1000).fadeIn(500);
-// 		});
-// 	})();
-// }
 
-// $(fadeInHeroHeading);
-
-// $(
-
-//     (function () {
-// 		$('.introduction h1').fadeIn(500, function () {
-// 			$('.introduction h2').delay(1000).fadeIn(500);
-// 		});
-// 	})();
-
-// );
 
 function fadeInIntro () {
     $('.hero-container h1').fadeIn(500, function () {
         $('.hero-container h2').delay(1000).fadeIn(500);
     });
+}
+
+function fadeNavOnScroll() {
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 30) {
+            $('#header').fadeOut(400);
+        } else {
+            $('#header').fadeIn(400);
+        }
+    });
     console.log('hello');
 }
 
-$(fadeInIntro);
+function smoothScroll() {
+    $('.links').find('a').click(function() {
+        let $href = $(this).attr('href');
+        let $anchor = $('#'+href).offset();
+        $('body').animate({ scrollTop: $anchor.top }, 3000);
+        return false;
+    });
+}
+    
+function handler () {
+    fadeInIntro();
+    fadeNavOnScroll();
+    smoothScroll();
+    console.log('hi');
+}
+
+$(handler);
